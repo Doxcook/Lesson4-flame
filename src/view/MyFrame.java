5f328df04005fc7f82f09c6de05fa817756743db
edpanov <egorpanov4002rus@gmail.com>
2024-09-26 09:09:55 +0300

    2

--- src/view/MyFrame.java
+++ src/view/MyFrame.java
@@ -1,8 +1,12 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
-
-//
+    public MyFrame() throws HeadlessException{
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500,500);
+        setVisible(true);
+    }
 }
