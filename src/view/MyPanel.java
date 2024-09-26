8ec648ebf9a8e7cbecf7548f264215bf3d24911b
edpanov <egorpanov4002rus@gmail.com>
2024-09-26 09:19:42 +0300

    1

---
+++ src/view/MyPanel.java
@@ -0,0 +1,9 @@
+package view;
+
+import javax.swing.*;
+
+public class MyPanel {
+    JTextField textField = new JTextField("   ");
+    JTextField textField1 = new JTextField("   ");
+    JButton button;
+}
