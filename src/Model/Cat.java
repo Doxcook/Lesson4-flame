7637525826d641872f7bfbad292492b1a602f25b
edpanov <egorpanov4002rus@gmail.com>
2024-09-26 12:57:04 +0300

    -

---
+++ src/Model/Cat.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Cat {
+    int age;
+    public Cat() {
+    }
+
+    public Cat(int age) {
+        this.age = age;
+    }
+}
---
+++ src/controller/Controller.java
@@ -0,0 +1,9 @@
+package controller;
+
+import Model.Cat;
+
+public class Controller {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+    }
+}
