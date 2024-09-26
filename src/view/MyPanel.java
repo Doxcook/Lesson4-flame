2bad6f948ebdae92c6aa88c04f25524fe694b145
edpanov <egorpanov4002rus@gmail.com>
2024-09-26 09:36:39 +0300

    it works2

--- src/view/MyPanel.java
+++ src/view/MyPanel.java
@@ -1,6 +1,8 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MyPanel extends JPanel {
     public MyPanel() {
@@ -10,6 +12,11 @@ public class MyPanel extends JPanel {
         add(textField);
         add(textField1);
         add(button);
-        button.addActionListener();
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
     }
 }
