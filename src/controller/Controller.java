9212f69f999954b21cc8dbe93bd802da4482310e
edpanov <egorpanov4002rus@gmail.com>
2024-09-26 12:59:22 +0300

    -

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,9 +1,14 @@
 package controller;
 
 import Model.Cat;
+import view.MyFrame;
+import view.MyPanel;
 
 public class Controller {
     public static void main(String[] args) {
         Cat cat = new Cat();
+        MyPanel panel = new MyPanel();
+        MyFrame frame = new MyFrame(panel);
+
     }
 }
